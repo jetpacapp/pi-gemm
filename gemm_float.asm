115a6177dc8ae0f93a479cd4907455c2594c325a
Pete Warden <pete@jetpac.com>
2014-07-15 15:19:16 -0700

    Fixed end-condition bug which skipped final TMU reads

--- gemm_float.asm
+++ gemm_float.asm
@@ -158,32 +158,32 @@ shl rAccum1, rAccum1, 2; nop
 add raTmu0S, rCurrentA, rAccum1; nop
 add raTmu0S, rCurrentB, rAccum1; nop
 
-ldi rAccum0, 64
+ldi rAccum2, 64
 
 ldi rL, 0
 main_loop_l:
 
 # See if we're out of 128-element chunks to process yet
-ldi rAccum2, ELEMENTS_PER_PASS_MINUS_ONE
-sub rAccum2, rK, rAccum2; nop
+ldi rAccum0, ELEMENTS_PER_PASS_MINUS_ONE
+sub rAccum0, rK, rAccum0; nop
 sub ra39, rL, rAccum2; nop
 brr.ne ra39, main_loop_l_break
 NOP
 NOP
 NOP
 
-add.ldtmu0 rCurrentA, rCurrentA, rAccum0; nop
+add.ldtmu0 rCurrentA, rCurrentA, rAccum2; nop
 or rA0to15, r4, 0; nop
 
 or.ldtmu0 ra39, ra39, ra39; nop
-add rCurrentB, rCurrentB, rAccum0; fmul rAccum2, rA0to15, r4
-fadd rTotal, rTotal, rAccum2; nop
+add rCurrentB, rCurrentB, rAccum2; fmul rAccum0, rA0to15, r4
+fadd rTotal, rTotal, rAccum0; nop
 
 add raTmu0S, rCurrentA, rAccum1; nop
 add raTmu0S, rCurrentB, rAccum1; nop
 
-ldi rAccum2, ELEMENTS_PER_PASS
-add rL, rL, rAccum2; nop
+ldi rAccum0, ELEMENTS_PER_PASS
+add rL, rL, rAccum0; nop
 brr ra39, main_loop_l
 NOP
 NOP
@@ -200,10 +200,10 @@ ldi rAccum0, 64
 or rAccum1, rLinearRamp, rLinearRamp; nop
 shl rAccum1, rAccum1, 2; nop
 
-add.ldtmu0 rCurrentA, rCurrentA, rAccum0; nop
+add.ldtmu0 rCurrentA, rCurrentA, rAccum2; nop
 or rA0to15, r4, 0; nop
 or.ldtmu0 ra39, ra39, ra39; nop
-add rCurrentB, rCurrentB, rAccum0; fmul rA0to15, rA0to15, r4
+add rCurrentB, rCurrentB, rAccum2; fmul rA0to15, rA0to15, r4
 
 ldi rMaskShift, 31
 ldi rElementsPerVector, 16
